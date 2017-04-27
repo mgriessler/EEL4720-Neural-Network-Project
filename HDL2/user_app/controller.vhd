29d1cfc82dab88e76298d123ea4b862ed3c60b33
Matthew Griessler <mgriessler@ufl.edu>
2017-04-27 15:19:28 -0400

    latest files now in HDL/user_app

--- HDL2/user_app/controller.vhd
+++ HDL2/user_app/controller.vhd
@@ -3,80 +3,94 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
 use work.config_pkg.all;
-use work.user_pkg.all;
 
 entity controller is
 port(
     clk : in std_logic;
     rst : in std_logic;
-    
+
+    --cpp control signals    
     go : in std_logic;
-    size : in std_logic_vector(C_MEM_ADDR_WIDTH downto 0);
+    epoch_size : in std_logic_vector(C_MMAP_DATA_WIDTH-1 downto 0);
     done : out std_logic;
     
-    mem_in_en : out std_logic;
-    mem_out_en : out std_logic;
+    --neural network control signals
+    nn_bp : in std_logic;
+    nn_init : out std_logic;
+    nn_en : out std_logic;
     
-    dp_en : out std_logic
+    --memory control signals
+    input_mems_rd_addr_inc : out std_logic;
+    output_mem_wr_addr_rst : out std_logic;
+    input_mems_rd_addr_rst : out std_logic
     
 );
 end controller;
 
 architecture cont of controller is
-signal state : std_logic_vector(2 downto 0);
+    type STATE_TYPE is (S_START, S_START_2, S_LOAD, S_FP, S_BP, S_FP_END_1, S_FP_END_2, S_DONE);
+    signal state, next_state : STATE_TYPE;
 signal count : integer := 0;
 begin
+    process(clk, rst)
+    begin
+        if(rst = '1') then
+            state <= S_START;
+        elsif(rising_edge(clk)) then
+            state <= next_state;
+        end if;
+    end process;
+    
+    process(state, nn_bp, go)
+        variable iterations : natural := 0;
+        variable first : std_logic := '1';
 
-process(clk, rst)
-begin
-if(rst = '1') then
-    state <= "000";
-elsif(clk = '1' and clk'event) then
-    if(state = "000") then
-        if(go = '1') then
-        state <= "001";
-        else
-        state <= "000";
-        end if;
-    end if;
-    
-    if(state = "001") then
-        if(count < 3) then
-            state <= "001";
-        else
-            state <= "010";
-        end if;
-        count <= count +1;
-    end if;
-    
-    if(state = "010") then
-        if(count < unsigned(size)) then
-            state <= "010";
-        else
-            state <= "011";
-        end if;
-        count <= count +1;
-    end if;
-    
-    if(state = "011") then
-        if(count < (3 + unsigned(size))) then
-            state <= "011";
-        else
-            state <= "100";
-        end if;
-        count <= count +1;
-    end if;
-    
-    if(state = "100") then
-        state <= "100";
-    end if;
-end if;
-end process;
-
-done <= '1' when state = "100" else '0';
-mem_in_en <= '1' when state = "001" or state = "010" else '0';
-mem_out_en <= '1' when state = "010" or state = "011" else '0';
-dp_en <= '1' when state = "001" or state = "010" or state = "011" else '0';
-
+    begin
+        nn_init <= '0';
+        nn_en <= '0';
+        input_mems_rd_addr_inc <= '0';
+        output_mem_wr_addr_rst <= '0';
+        input_mems_rd_addr_rst <= '0';
+        done <= '0';
+        case state is
+            when S_START =>
+                if(go = '1') then
+                    next_state <= S_START_2;
+                else
+                    next_state <= S_START;
+                end if;
+            when S_START_2 =>
+                output_mem_wr_addr_rst <= '1';
+                input_mems_rd_addr_rst <= '1';
+                nn_init <= '1';
+                iterations := 0;
+                next_state <= S_FP;
+            when S_FP =>
+                nn_en <= '1';                
+                iterations := iterations + 1;
+                if(iterations = unsigned(epoch_size)) then
+                    next_state <= S_FP_END_1;
+                else
+                    next_state <= S_FP;
+                end if;
+                input_mems_rd_addr_inc <= '1';
+            when S_BP =>
+                nn_en <= '1';                
+                -- do back prop
+                next_state <= S_DONE; --temp
+            when S_FP_END_1 =>
+                nn_en <= '1';
+                next_state <= S_FP_END_2;
+            when S_FP_END_2 =>
+                nn_en <= '1';
+                next_state <= S_DONE;
+            when S_DONE =>
+                done <= '1';
+                if(go = '0') then
+                    next_state <= S_START;
+                else
+                    next_state <= S_DONE;
+                end if;
+            end case;
+    end process;
 end cont;
-
--- HDL2/user_app/user_app.vhd
+++ HDL2/user_app/user_app.vhd
@@ -43,6 +43,13 @@ architecture default of user_app is
     signal output_out_data : DBL_DATA;
     signal output_wr_en : std_logic := '0';
     
+    signal input_mems_rd_addr : std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);
+    signal output_mem_wr_addr : std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);
+    
+    signal ctrl_go : std_logic;
+    signal ctrl_epoch_size : std_logic_vector(C_MMAP_DATA_WIDTH-1 downto 0);
+    signal ctrl_done : std_logic;
+    
 begin
 
     U_MEM_MAP : entity work.mem_map(BHV)
@@ -55,6 +62,9 @@ begin
         rd_en => mmap_rd_en,
         rd_addr => mmap_rd_addr,
         rd_data => mmap_rd_data,
+        go => ctrl_go,
+        epoch_size => ctrl_epoch_size,
+        done => ctrl_done,
         mem_in_addr => mem_in_addr,
         mem_out_addr => mem_out_addr,
         mem_in_data => mem_in_data,
@@ -74,21 +84,6 @@ begin
         output_wr_en => output_wr_en
     );
         
-    U_MEMTEST : entity work.ram(SYNC_READ)
-    generic map (
-        num_words => 2**MEM_ADDR_WIDTH,
-        word_width => MEM_DATA_WIDTH,
-        addr_width => MEM_ADDR_WIDTH
-    )
-    port map (
-        clk => clk,
-        wen => memtest_wr_en,
-        waddr => mem_in_addr(MEM_ADDR_WIDTH-1 downto 0),
-        wdata => mem_in_data,
-        raddr => mem_out_addr(MEM_ADDR_WIDTH-1 downto 0),
-        rdata => memtest_out_data
-    );
-
     U_INPUT_0 : entity work.ram(SYNC_READ)
     generic map (
         num_words => 2**MEM_ADDR_WIDTH,
@@ -100,7 +95,7 @@ begin
         wen => input_0_wr_en,
         waddr => mem_in_addr(MEM_ADDR_WIDTH-1 downto 0),
         wdata => mem_in_data,
-        raddr => mem_out_addr(MEM_ADDR_WIDTH-1 downto 0),
+        raddr => input_mems_rd_addr,
         rdata => input_0_out_data
     );
 
@@ -115,7 +110,7 @@ begin
         wen => input_1_wr_en,
         waddr => mem_in_addr(MEM_ADDR_WIDTH-1 downto 0),
         wdata => mem_in_data,
-        raddr => mem_out_addr(MEM_ADDR_WIDTH-1 downto 0),
+        raddr => input_mems_rd_addr,
         rdata => input_1_out_data
     );
 
@@ -130,7 +125,7 @@ begin
         wen => input_2_wr_en,
         waddr => mem_in_addr(MEM_ADDR_WIDTH-1 downto 0),
         wdata => mem_in_data,
-        raddr => mem_out_addr(MEM_ADDR_WIDTH-1 downto 0),
+        raddr => input_mems_rd_addr,
         rdata => input_2_out_data
     );
 
@@ -145,7 +140,7 @@ begin
         wen => input_3_wr_en,
         waddr => mem_in_addr(MEM_ADDR_WIDTH-1 downto 0),
         wdata => mem_in_data,
-        raddr => mem_out_addr(MEM_ADDR_WIDTH-1 downto 0),
+        raddr => input_mems_rd_addr,
         rdata => input_3_out_data
     );
 
@@ -160,7 +155,7 @@ begin
         wen => expected_output_wr_en,
         waddr => mem_in_addr(MEM_ADDR_WIDTH-1 downto 0),
         wdata => mem_in_data,
-        raddr => mem_out_addr(MEM_ADDR_WIDTH-1 downto 0),
+        raddr => input_mems_rd_addr,
         rdata => expected_output_out_data
     );
 
@@ -173,10 +168,39 @@ begin
     port map (
         clk => clk,
         wen => output_wr_en,
-        waddr => mem_in_addr(MEM_ADDR_WIDTH-1 downto 0),
+        waddr => output_mem_wr_addr,
         wdata => mem_in_data,
         raddr => mem_out_addr(MEM_ADDR_WIDTH-1 downto 0),
         rdata => output_out_data
     );
+    
+    U_CNTRL : entity work.controller(cont)
+    port map (
+        clk => clk,
+        rst => rst,
+        go => ctrl_go,
+        epoch_size => ctrl_epoch_size,
+        done => ctrl_done,
+        nn_bp => nn_bp,
+        nn_init => nn_init,
+        nn_en => nn_en,
+        input_mems_rd_addr_inc => input_mems_rd_addr_inc,
+        output_mem_wr_addr_rst => output_mem_wr_addr_rst,
+        input_mems_rd_addr_rst => input_mems_rd_addr_rst
+    );
 
+    U_MEMTEST : entity work.ram(SYNC_READ)
+    generic map (
+        num_words => 2**MEM_ADDR_WIDTH,
+        word_width => MEM_DATA_WIDTH,
+        addr_width => MEM_ADDR_WIDTH
+    )
+    port map (
+        clk => clk,
+        wen => memtest_wr_en,
+        waddr => mem_in_addr(MEM_ADDR_WIDTH-1 downto 0),
+        wdata => mem_in_data,
+        raddr => mem_out_addr(MEM_ADDR_WIDTH-1 downto 0),
+        rdata => memtest_out_data
+    );
 end default;
